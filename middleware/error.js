7eee45ddfe768471d3b33cd7e3896d45f8e4b869
CoderGuy614 <jlutz.110@gmail.com>
2020-05-19 15:48:18 +0800

    Async error handler

---
+++ middleware/error.js
@@ -0,0 +1,33 @@
+const ErrorResponse = require("../utils/errorResponse");
+const errorHandler = (err, req, res, next) => {
+  let error = { ...err };
+  error.message = err.message;
+
+  // Log to console for developer
+  console.log(err.stack.red);
+
+  //Mongoose Bad Id
+  if (err.name === "CastError") {
+    const message = `Bootcamp not found with id of ${err.value}`;
+    error = new ErrorResponse(message, 404);
+  }
+
+  //Mongoose Duplicate Key
+  if (err.code === 11000) {
+    const message = "Duplicate field value entered";
+    error = new ErrorResponse(message, 400);
+  }
+
+  res.status(error.statusCode || 500).json({
+    success: false,
+    error: error.message || "Server Error",
+  });
+
+  // Mongoose validation error
+  if (err.name === "ValidationError") {
+    const message = Object.values(err.errors).map((val) => val.message);
+    error = new ErrorResponse(message, 400);
+  }
+};
+
+module.exports = errorHandler;
